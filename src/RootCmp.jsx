b0b0e113f796554bae5f8e71dd425518b3f33712
Dan Tayari <kisa@tayari.co.il>
2025-03-05 20:01:56 +0200

    init

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -7,38 +7,8 @@ import { Routes, Route } from 'react-router'
 import { AppHeader } from './cmps/AppHeader.jsx'
 import { Home } from './pages/Home.jsx'
 import { Footer } from './cmps/Footer.jsx'
-// import { AppFooter } from './cmps/AppFooter'
-// import { UserMsg } from './cmps/UserMsg.jsx'
-// import { SideBar } from './cmps/SideBar.jsx'
-// import HiddenReactPlayer from './cmps/HiddenReactPlayer'
-// import { playerSongEndedEvent } from './store/actions/player.actions.js'
-
-// import { Login } from './pages/Login.jsx'
-// import { StationIndex } from './pages/StationIndex.jsx'
-// import { StationDetails } from './pages/StationDetails.jsx'
-// import { StationExplore } from './pages/StationExplore.jsx'
-// // import { StationSearch } from './pages/StationSearch.jsx'
-
-// // Init db
-// import {} from './services/station'
-// import {} from './services/song'
-// import {} from './services/youtube-api.service.js'
-// import { login } from './store/actions/user.actions.js'
-// import { loadStations, setLikedSongs } from './store/actions/station.actions.js'
 
 export function RootCmp() {
-  // const playerRef = useRef(null)
-  // const mainRef = useRef(null)
-
-  // const handleSongEnded = () => {
-  //   playerSongEndedEvent()
-  // }
-
-  useEffect(() => {
-    // Connect default guest
-    // login({ username: 'guest', password: 'guest' })
-  }, [])
-
   return (
     <div className='main-container main-layout'>
       <AppHeader />
